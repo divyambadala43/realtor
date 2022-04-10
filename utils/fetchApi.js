cbf11e6021041c956d43a95981412c583e93b6d5
Divya <divyambadala43@gmail.com>
2022-04-10 13:11:38 +0530

    FINAL COMMIT: DONE

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
 const API = process.env.rapidApiKey;
-console.log(API);
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      "X-RapidAPI-Key": "d57b1a3e6dmshf74cb51c7d772a7p1f47c5jsn9e04c7903b05",
+      "X-RapidAPI-Key": API,
     },
   });
   return data;
